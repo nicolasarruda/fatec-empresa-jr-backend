4e5156e8429ddbcb848a25d22804c806c114f810
nicolasarruda <nlarruda@hotmail.com>
2021-10-01 20:35:01 -0300

    Starting the project

---
+++ src/main/java/com/empresajr/fatec/FatecApplication.java
@@ -0,0 +1,13 @@
+package com.empresajr.fatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresajr/fatec/FatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresajr.fatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
