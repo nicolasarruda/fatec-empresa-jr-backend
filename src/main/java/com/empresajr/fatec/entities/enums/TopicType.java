fc3f5298d03bb35eb8960f8cacdbd61093c30337
nicolasarruda <nlarruda@hotmail.com>
2021-10-25 20:52:39 -0300

    Refactoring Person, Author, User and Type

--- src/main/java/com/empresajr/fatec/entities/enums/TopicType.java
+++ src/main/java/com/empresajr/fatec/entities/enums/TopicType.java
@@ -7,8 +7,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Type {
 
-    USER(1),
-    AUTHOR(2);
+    NEOTECH(1),
+    ADS(2),
+    EVENTOS(3),
+    GESTAO_AMBIENTAL(4),
+    GESTAO_EMPRESARIAL(5),
+    GTI(6),
+    LOGISTICA(7),
+    SISTEMAS_EMBARCADOS(8);
 
     private final int type;
 }
